9d22a0fd47881e7a6f6bf8c38935b0027e6e4e36
Peer Heinen <peer.heinen@gmail.com>
2020-03-28 17:53:51 +0100

    updates from second stream

---
+++ ep02/portscanner/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	_, err := net.Dial("tcp", "scanme.nmap.org:80")
+	if err == nil {
+		fmt.Println("Connection successful")
+	}
+	if err != nil {
+		fmt.Print(err)
+	}
+}
---
+++ ep02/portscanner2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		fmt.Print(address)
+		if err != nil {
+			// port is closed or filtered.
+			continue
+		}
+		conn.Close()
+		fmt.Printf("%d open\n", i)
+	}
+}
